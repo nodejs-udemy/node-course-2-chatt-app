9e21bace3707ca2007cfb7a612493fdd39dab316
nicole <nodejs@gmx.de>
2017-05-07 13:34:21 +0200

    Emit new Message on createMessage

--- server/server.js
+++ server/server.js
@@ -14,6 +14,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log(`New user connected`);
 
+  socket.on('createMessage', (message) => {
+    console.log(`createMessage: ${JSON.stringify(message)}`);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
   socket.on('disconnect', (socket) => {
     console.log('Client disconnected');
   });
